1062d3034b31b7c2b9a54ca2558cc55c8de131b0
zjpeters <zeru-peterson@uiowa.edu>
2022-03-03 08:32:09 -0600

    got to run on multiple samples

--- code/extractSpatialInfoPython220215.py
+++ code/extractSpatialInfoPython220215.py
@@ -27,13 +27,20 @@ from allensdk.core.reference_space_cache import ReferenceSpaceCache
 derivatives = "../derivatives"
 rawdata = "../rawdata"
 
+
+# import allen data finish "chooseTemplateSlice" from this code
+# ara_nissl_10 is 10 um, ara_nissl_100 is 100um
+# this one takes awhile, so don't rerun often
+ara_data = ants.image_read("../data/ccf/ara_nissl_10.nrrd")
+annotation_data = ants.image_read("../data/ccf/annotation_10.nrrd")
+#%%
 # imports the "tissue_hires_image.png" output to register to allen ccf
 def importVisiumData(sampleFolder):
     # this currently assumes that sampleFolder contains spatial folder and the
     # filtered_feature_bc_matrix.h5 output from space ranger
     visiumData = {}
     visiumData['imageData'] = io.imread(os.path.join(sampleFolder,"spatial","tissue_hires_image.png"), as_gray=True)
-    
+    visiumData['sampleID'] = sampleFolder.rsplit(sep='/',maxsplit=1)[-1]
     tissuePositionsList = []
     tissueSpotBarcodes = []
     with open(os.path.join(sampleFolder,"spatial","tissue_positions_list.csv"), newline='') as csvfile:
@@ -48,6 +55,7 @@ def importVisiumData(sampleFolder):
     visiumData['tissuePositionsList'] = tissuePositionsList
     scaleFactorPath = open(os.path.join(sampleFolder,"spatial","scalefactors_json.json"))
     visiumData['scaleFactors'] = json.loads(scaleFactorPath.read())
+    scaleFactorPath.close()
 #    filteredFeatureMatrixPath = glob(os.path.join(sampleFolder,"*_filtered_feature_bc_matrix.h5"))
 #    filteredFeatureMatrix = get_matrix_from_h5.get_matrix_from_h5(filteredFeatureMatrixPath)
 
@@ -56,6 +64,7 @@ def importVisiumData(sampleFolder):
 # prepares visium data for registration
 def processVisiumData(visiumSample):
     processedVisium = {}
+    # the sampleID might have issues on non unix given the slash direction, might need to fix
     processedVisium['visiumGauss'] = filters.gaussian(visiumSample['imageData'], sigma=20)
     processedVisium['otsuThresh'] = filters.threshold_otsu(processedVisium['visiumGauss'])
     processedVisium['visiumOtsu'] = processedVisium['visiumGauss'] < processedVisium['otsuThresh']
@@ -64,6 +73,10 @@ def processVisiumData(visiumSample):
     io.imshow(visiumSample['imageData'])
     plt.show()
     io.imshow(processedVisium['tissue'])
+    outputPath = os.path.join(derivatives, visiumSample['sampleID'])
+    if not os.path.exists(outputPath):
+        os.mkdir(outputPath)
+    processedVisium['derivativesPath'] = outputPath
     return processedVisium
 
 # select which allen slice to align visium data to and import relevant data
@@ -76,7 +89,7 @@ def chooseTemplateSlice(sliceLocation, resolution):
         templateAnnotationSlice = annotation_data[sliceLocation,:,:]
         templateLeftSlice = templateSlice[:,0:templateSlice.shape[1]//2]
         templateRightSlice = templateSlice[:,(templateSlice.shape[1]//2+1):]
-        templateStartingResolution = 0.1\
+        templateStartingResolution = 0.1
         # io.imshow(templateLeftSlice)
     elif resolution == 10:
         ara_data = ants.image_read("../data/ccf/ara_nissl_10.nrrd")
@@ -98,21 +111,24 @@ def chooseTemplateSlice(sliceLocation, resolution):
     return templateSlice, templateLeftSlice, templateRightSlice, templateAnnotationSlice, templateStartingResolution
 #
 #allenSlice73, allenLeftSlice73, allenRightSlice73, allenAnnotSlice73 = chooseTemplateSlice(73, 10)
-#%% import allen data
-# this one takes awhile, so don't rerun often
-ara_data = ants.image_read("../data/ccf/ara_nissl_10.nrrd")
-annotation_data = ants.image_read("../data/ccf/annotation_10.nrrd")
-#%%, finish "chooseTemplateSlice" from this code
-# ara_nissl_10 is 10 um, ara_nissl_100 is 100um
-templateStartingResolution = 0.01
-templateSlice = ara_data.slice_image(0,650)
-templateAnnotationSlice = annotation_data.slice_image(0,650)
 
+#%% choose template slice
+
+# list of best template slices for each sample could be a good inclusion in participants.tsv, if using format
+# sample-01: 68, sample-02: 66, sample-03: na, sample-04: 72, sample-05 72, sample-06: 66
+bestSlice = 66
+# sample-01: 270, sample-02: 270, sample-03: na, sample-04: 0, sample-05: 90, sample-06: 90
+# should be able to do away with this by implementing the fiducial alignment
+degreesToRotate = 90
+# below is set up for 10um
+templateStartingResolution = 0.01
+templateSlice = ara_data.slice_image(0,(bestSlice*10))
+templateAnnotationSlice = annotation_data.slice_image(0,(bestSlice*10))
+
+# this cuts the image in half to use only the left/right hemisphere
 templateLeft = templateSlice[:,571:]
 templateAnnotationLeft = templateAnnotationSlice[:,571:]
 templateLeft = cv2.normalize(templateLeft, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-
-#%%
 
 templateLeftGauss = filters.gaussian(templateLeft, 10)
 io.imshow(templateLeftGauss)
@@ -121,12 +137,12 @@ io.imshow(templateLeftGauss)
 sampleStartingResolution = 6.5 / 1560
 resolutionRatio = sampleStartingResolution / templateStartingResolution
 
-sample = importVisiumData("../rawdata/sleepDepBothBatches/sample-01")
+sample = importVisiumData("../rawdata/sleepDepBothBatches/sample-07")
 sampleProcessed = processVisiumData(sample)
 #%% 
 sampleNorm = cv2.normalize(sampleProcessed["tissue"], None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
 # (np.sum((templateLeft > 0)/np.sum(sampleNorm > 0))) for below
-degreesToRotate = -90
+
 sampleResize = rescale(sampleNorm,resolutionRatio)
 # if resize=True not set, image will be slightly misaligned with spots
 sampleRotate = rotate(sampleResize, degreesToRotate, resize=True)
@@ -139,14 +155,23 @@ sampleAntsImage = ants.from_numpy(sampleHistMatch)
 # templateAntsImage.set_spacing([templateStartingResolution,templateStartingResolution])
 # sampleAntsImage.set_spacing([templateStartingResolution,templateStartingResolution])
 
-synXfm = ants.registration(fixed=templateAntsImage, moving=sampleAntsImage, type_of_transform='SyN', outprefix='../derivatives/sample-01_xfm')
+synXfm = ants.registration(fixed=templateAntsImage, moving=sampleAntsImage, type_of_transform='SyN', outprefix=os.path.join(sampleProcessed['derivativesPath'],f"{sample['sampleID']}_xfm"))
 ants.plot(templateAntsImage, overlay=synXfm["warpedmovout"])
+
+#%%
+# CHECK FOR ACCURACY OF ABOVE REGISTRATION
+#
 
 #%% Tissue point registration
 # update tissue points with pre-registration alignment of sample image
 # -90 = [[0,1],[-1,0]]
 rotMat = [[0,1],[-1,0]]
-
+if degreesToRotate == 0:
+    rotMat = [[1,0],[0,1]]
+elif degreesToRotate == 90:
+    rotMat = [[0,-1],[1,0]]
+elif degreesToRotate == 270:
+    rotMat = [[0,1],[-1,0]]
 tissuePointsResizeToHighRes = sample["tissuePositionsList"][0:, 3:] * sample["scaleFactors"]["tissue_hires_scalef"]
 # below switches x and y in order to properly rotate, this gets undone in next cell
 tissuePointsResizeToHighRes[:,[0,1]] = tissuePointsResizeToHighRes[:,[1,0]]
@@ -156,7 +181,12 @@ plt.show()
 
 tissuePointsResizeRotate = np.matmul(tissuePointsResizeToHighRes, rotMat)
 # below accounts for shift resulting from matrix rotation above, will be different for different angles
-tissuePointsResizeRotate[:,0] = tissuePointsResizeRotate[:,0] + sampleProcessed["tissue"].shape[0]
+if degreesToRotate == 0:
+    tissuePointsResizeRotate[:,0] = tissuePointsResizeRotate[:,0]
+elif degreesToRotate == 90:
+    tissuePointsResizeRotate[:,1] = tissuePointsResizeRotate[:,1] + sampleProcessed["tissue"].shape[1]
+elif degreesToRotate == 270:
+    tissuePointsResizeRotate[:,0] = tissuePointsResizeRotate[:,0] + sampleProcessed["tissue"].shape[0]
 tissuePointsResizeToTemplate = tissuePointsResizeRotate * resolutionRatio
 plt.imshow(sampleRotate)
 plt.plot(tissuePointsResizeToTemplate[:,0],tissuePointsResizeToTemplate[:,1],marker='.', c='red', alpha=0.2)
@@ -169,12 +199,12 @@ tissuePointsResizeToTemplate[:,[0,1]] = tissuePointsResizeToTemplate[:,[1,0]]
 
 csvPad = np.zeros([tissuePointsResizeToTemplate.shape[0],4])
 tissuePointsForTransform = np.append(tissuePointsResizeToTemplate, csvPad, 1)
-np.savetxt('../derivatives/sample-01_tissuePointsResizeToTemplate.csv',tissuePointsForTransform, delimiter=',', header="x,y,z,t,label,comment")
-os.system("antsApplyTransformsToPoints -d 2 -i ../derivatives/sample-01_tissuePointsResizeToTemplate.csv -o ../derivatives/sample-01_tissuePointsResizeToTemplateTransformApplied.csv -t [ ../derivatives/sample-01_xfm0GenericAffine.mat,1] -t ../derivatives/sample-01_xfm1InverseWarp.nii.gz")
+np.savetxt(f"{os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_tissuePointsResizeToTemplate.csv",tissuePointsForTransform, delimiter=',', header="x,y,z,t,label,comment")
+os.system(f"antsApplyTransformsToPoints -d 2 -i {os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_tissuePointsResizeToTemplate.csv -o {os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_tissuePointsResizeToTemplateTransformApplied.csv -t [ {os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_xfm0GenericAffine.mat,1] -t {os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_xfm1InverseWarp.nii.gz")
 
 #%% open and check transformed coordinates
 transformedTissuePositionList = []
-with open(os.path.join('../derivatives/sample-01_tissuePointsResizeToTemplateTransformApplied.csv'), newline='') as csvfile:
+with open(os.path.join(f"{os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_tissuePointsResizeToTemplateTransformApplied.csv"), newline='') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
         next(csvreader)
         for row in csvreader:
@@ -213,13 +243,23 @@ plt.scatter(transformedTissuePositionListFinal[0:,0],transformedTissuePositionLi
 plt.show()
 plt.imshow
 
-# create a "fake" annotation image that just looks better in an overlay
+# create a "fake" annotation image by replacing all regions with # > 1500 with one value that just looks better in an overlay
 templateAnnotationLeftFake = templateAnnotationLeft
 templateAnnotationLeftFake[templateAnnotationLeft > 1500] = 100
 plt.imshow(templateAnnotationLeft)
 plt.scatter(transformedTissuePositionListFinal[0:,0],transformedTissuePositionListFinal[0:,1], marker='.', c='red', alpha=0.5)
 plt.show()
 plt.imshow
+
+#%%
+plt.imshow(sampleTransformed)
+plt.scatter(transformedTissuePositionListFinal[0:,0],transformedTissuePositionListFinal[0:,1], marker='x', c='red', alpha=0.3)
+plt.show()
+plt.imshow
+
+plt.imshow(sampleTransformed,cmap='gray')
+plt.imshow(templateAnnotationLeft, alpha=0.3)
+plt.show()
 #%% extract atlas information
 reference_space_key = 'annotation/ccf_2017'
 resolution = 10
