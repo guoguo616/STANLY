400def2638053369c768e239a37d28ca1279e9e2
zjpeters <zeru-peterson@uiowa.edu>
2022-03-16 13:59:24 -0500

    have the major functions defined and running for single samples, ready to start with multi-sample

--- code/extractSpatialInfoPython220315.py
+++ code/extractSpatialInfoPython220315.py
@@ -5,10 +5,10 @@ Created on Mon Feb  7 09:49:40 2022
 
 @author: zjpeters
 """
-#import skimage
+
+# data being read in includes: json, h5, csv, nrrd, jpg, and svg
 from skimage import io, filters
 from skimage.transform import rescale, rotate
-# from skimage import exposure
 from skimage.exposure import match_histograms
 from matplotlib import pyplot as plt
 import os
@@ -16,25 +16,30 @@ import numpy as np
 import json
 import csv
 import cv2
-#import h5py
-#mport get_matrix_from_h5
 from glob import glob
 import ants
 
-
 # setting up paths
-# needs to read json, h5, jpg, and svg
 derivatives = "../derivatives"
-rawdata = "../rawdata"
+rawdata = "../rawdata/sleepDepBothBatches"
 
-
-# import allen data finish "chooseTemplateSlice" from this code
+# need to think about best way to load allen data given the size
 # ara_nissl_10 is 10 um, ara_nissl_100 is 100um
 # this one takes awhile, so don't rerun often
 ara_data = ants.image_read("../data/ccf/ara_nissl_10.nrrd")
 annotation_data = ants.image_read("../data/ccf/annotation_10.nrrd")
 
-"""
+""" notes about visium data:
+    there are a total of 4,992 possible spots on a slide
+    tissue_positions_list.csv contains:
+    barcode: The sequence of the barcode associated to the spot.
+    in_tissue: Binary, indicating if the spot falls inside (1) or outside (0) of tissue.
+    array_row: The row coordinate of the spot in the array from 0 to 77. The array has 78 rows.
+    array_col: The column coordinate of the spot in the array. In order to express the orange crate arrangement of the spots, this column index uses even numbers from 0 to 126 for even rows, and odd numbers from 1 to 127 for odd rows. Notice then that each row (even or odd) has 64 spots.
+    pxl_row_in_fullres: The row pixel coordinate of the center of the spot in the full resolution image.
+    pxl_col_in_fullres: The column pixel coordinate of the center of the spot in the full resolution image.
+
+
 get_matrix_from_h5 is from code @:
     https://support.10xgenomics.com/spatial-gene-expression/software/pipelines/latest/advanced/h5_matrices
 """
@@ -72,17 +77,14 @@ end of code from 10x
 """
 
 #%%
-# imports the "tissue_hires_image.png" output to register to allen ccf
-""" notes about visium data:
-    there are a total of 4,992 possible spots on a slide
-    tissue_positions_list.csv contains:
-    barcode: The sequence of the barcode associated to the spot.
-    in_tissue: Binary, indicating if the spot falls inside (1) or outside (0) of tissue.
-    array_row: The row coordinate of the spot in the array from 0 to 77. The array has 78 rows.
-    array_col: The column coordinate of the spot in the array. In order to express the orange crate arrangement of the spots, this column index uses even numbers from 0 to 126 for even rows, and odd numbers from 1 to 127 for odd rows. Notice then that each row (even or odd) has 64 spots.
-    pxl_row_in_fullres: The row pixel coordinate of the center of the spot in the full resolution image.
-    pxl_col_in_fullres: The column pixel coordinate of the center of the spot in the full resolution image.
-
+# imports the "tissue_hires_image.png" output from spaceranger to register to allen ccf
+""" the general workflow should go as follows:
+    1. import visium data that has been run through spaceranger pipeline
+    2. import relevant atlas images and annotations from allen atlas
+    3. prepare visium data for registration into Common Coordinate Framework (ccf)
+    4. use SyN registration from ANTs to register visium image to allen image
+    5. bring remaining visium data, such as spot coordinates, into allen space using above transformations
+    6. measure for nearest neighbor similarity among spots in new space
 """
 def importVisiumData(sampleFolder):
     # this currently assumes that sampleFolder contains spatial folder and the
@@ -108,30 +110,14 @@ def importVisiumData(sampleFolder):
     filteredFeatureMatrixPath = glob(os.path.join(sampleFolder,"*_filtered_feature_bc_matrix.h5"))
     filteredFeatureMatrix = get_matrix_from_h5(os.path.join(filteredFeatureMatrixPath[0]))
     visiumData['filteredFeatureMatrix'] = filteredFeatureMatrix
+    # the ratio of real spot diameter, 55um, by imaged resolution of spot
+    visiumData['sampleStartingResolution'] = 0.55 / visiumData["scaleFactors"]["spot_diameter_fullres"]
 
     return visiumData
 
-# prepares visium data for registration
-def processVisiumData(visiumSample):
-    processedVisium = {}
-    # the sampleID might have issues on non unix given the slash direction, might need to fix
-    processedVisium['visiumGauss'] = filters.gaussian(visiumSample['imageData'], sigma=20)
-    processedVisium['otsuThresh'] = filters.threshold_otsu(processedVisium['visiumGauss'])
-    processedVisium['visiumOtsu'] = processedVisium['visiumGauss'] < processedVisium['otsuThresh']
-    processedVisium['tissue'] = np.zeros(processedVisium['visiumGauss'].shape)
-    processedVisium['tissue'][processedVisium['visiumOtsu']==True] = visiumSample['imageData'][processedVisium['visiumOtsu']==True]
-    io.imshow(visiumSample['imageData'])
-    plt.show()
-    io.imshow(processedVisium['tissue'])
-    outputPath = os.path.join(derivatives, visiumSample['sampleID'])
-    if not os.path.exists(outputPath):
-        os.mkdir(outputPath)
-    processedVisium['derivativesPath'] = outputPath
-    return processedVisium
-
 # select which allen slice to align visium data to and import relevant data
 def chooseTemplateSlice(sliceLocation):
-    templateInfo = {}
+    templateData = {}
     bestSlice = sliceLocation * 10
     templateSlice = ara_data.slice_image(0,(bestSlice))
     templateAnnotationSlice = annotation_data.slice_image(0,(bestSlice))
@@ -143,104 +129,125 @@ def chooseTemplateSlice(sliceLocation):
     templateAnnotationRight = templateAnnotationSlice[:,:570]
     # templateLeftSliceGauss = filters.gaussian(templateLeftSlice, 10)
     # templateRightSliceGauss = filters.gaussian(templateRightSlice, 10)
-    templateInfo['sliceNumber'] = sliceLocation
-    templateInfo['leftHem'] = templateLeft
-    templateInfo['rightHem'] = templateRight
-    templateInfo['leftHemAnnot'] = templateAnnotationLeft
-    templateInfo['rightHemAnnot'] = templateAnnotationRight
-    templateInfo['startingResolution'] = 0.01
+    templateData['sliceNumber'] = sliceLocation
+    templateData['leftHem'] = templateLeft
+    templateData['rightHem'] = templateRight
+    templateData['leftHemAnnot'] = templateAnnotationLeft
+    templateData['rightHemAnnot'] = templateAnnotationRight
+    # currently using the 10um resolution atlas, would need to change if that changes
+    templateData['startingResolution'] = 0.01
     
-    return templateInfo
-#%%
+    return templateData
+
+# tissue coordinates should reference output of importVisiumData
+# rotation currently accepts 0,90,180,270, will take input from processedVisium
+def rotateTissuePoints(visiumData, rotation):
+    # scales tissue coordinates down to image resolution
+    tissuePointsResizeToHighRes = visiumData["tissuePositionsList"][0:, 3:] * visiumData["scaleFactors"]["tissue_hires_scalef"]
+    # below switches x and y in order to properly rotate, this gets undone after registration
+    tissuePointsResizeToHighRes[:,[0,1]] = tissuePointsResizeToHighRes[:,[1,0]]  
+    # below rotates coordinates and accounts for shift resulting from matrix rotation above, will be different for different angles
+    # since the rotation is happening in euclidean space, we have to bring the coordinates back to image space
+    if degreesToRotate == 0:
+        # a null step, but makes for continuous math
+        rotMat = [[1,0],[0,1]]
+        tissuePointsResizeRotate = np.matmul(tissuePointsResizeToHighRes, rotMat)
+        tissuePointsResizeRotate[:,0] = tissuePointsResizeRotate[:,0]
+    elif degreesToRotate == 90:
+        rotMat = [[0,-1],[1,0]]
+        tissuePointsResizeRotate = np.matmul(tissuePointsResizeToHighRes, rotMat)
+        tissuePointsResizeRotate[:,1] = tissuePointsResizeRotate[:,1] + visiumData["imageData"].shape[1]
+    elif degreesToRotate == 180:
+        rotMat = [[-1,0],[0,-1]]
+        tissuePointsResizeRotate = np.matmul(tissuePointsResizeToHighRes, rotMat)
+        tissuePointsResizeRotate[:,0] = tissuePointsResizeRotate[:,0] + visiumData["imageData"].shape[0]
+        tissuePointsResizeRotate[:,1] = tissuePointsResizeRotate[:,1] + visiumData["imageData"].shape[1]
+    elif degreesToRotate == 270:
+        rotMat = [[0,1],[-1,0]]
+        tissuePointsResizeRotate = np.matmul(tissuePointsResizeToHighRes, rotMat)
+        tissuePointsResizeRotate[:,0] = tissuePointsResizeRotate[:,0] + visiumData["imageData"].shape[0]
+    else:
+        print("Incorrect rotation! Please enter: 0, 90, 180, or 270")
+    
+    return tissuePointsResizeRotate
+
+# prepares visium data for registration
+def processVisiumData(visiumData, templateData, rotation):
+    processedVisium = {}
+    # the sampleID might have issues on non unix given the slash direction, might need to fix
+    processedVisium['degreesOfRotation'] = rotation
+    processedVisium['visiumGauss'] = filters.gaussian(visiumData['imageData'], sigma=20)
+    processedVisium['otsuThreshold'] = filters.threshold_otsu(processedVisium['visiumGauss'])
+    processedVisium['visiumOtsu'] = processedVisium['visiumGauss'] < processedVisium['otsuThreshold']
+    processedVisium['tissue'] = np.zeros(processedVisium['visiumGauss'].shape)
+    processedVisium['tissue'][processedVisium['visiumOtsu']==True] = visiumData['imageData'][processedVisium['visiumOtsu']==True]
+    plt.imshow(visiumData['imageData'])
+    plt.show()
+    outputPath = os.path.join(derivatives, visiumData['sampleID'])
+    if not os.path.exists(outputPath):
+        os.mkdir(outputPath)
+    processedVisium['derivativesPath'] = outputPath
+    processedVisium['tissueNormalized'] = cv2.normalize(processedVisium['tissue'], None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+    processedVisium['resolutionRatio'] = visiumData['sampleStartingResolution'] / templateData['startingResolution']
+    processedVisium['tissueResized'] = rescale(processedVisium['tissueNormalized'],processedVisium['resolutionRatio'])
+    processedVisium['tissueRotated'] = rotate(processedVisium['tissueResized'], rotation, resize=True)
+    processedVisium['tissueHistMatched'] = match_histograms(processedVisium['tissueRotated'], templateData['leftHem'])
+    processedVisium['tissuePointsRotated'] = rotateTissuePoints(visiumData, rotation)
+    processedVisium['tissuePointsResized'] = processedVisium['tissuePointsRotated'] * processedVisium['resolutionRatio']
+    processedVisium['tissuePointsResizedForTransform'] = processedVisium['tissuePointsRotated'] * processedVisium['resolutionRatio']
+    processedVisium['tissuePointsResizedForTransform'][:,[0,1]] = processedVisium['tissuePointsResizedForTransform'][:,[1,0]]
+    csvPad = np.zeros([processedVisium['tissuePointsResizedForTransform'].shape[0],4])
+    processedVisium['tissuePointsForTransform'] = np.append(processedVisium['tissuePointsResizedForTransform'], csvPad, 1)
+    plt.imshow( processedVisium['tissueRotated'])
+    plt.plot(processedVisium['tissuePointsResized'][:,0],processedVisium['tissuePointsResized'][:,1],marker='.', c='red', alpha=0.2)
+    plt.show()
+    return processedVisium
+
+#%% import sample list, location, and degrees of rotation from participants.tsv
+# sampleList contains sample ids, templateList contains template slices and degrees of rotation to match
 sampleList = []
 templateList = []
-# rotationList = []
-
 with open(os.path.join(rawdata,"participants.tsv"), newline='') as tsvfile:
     tsvreader = csv.reader(tsvfile, delimiter='\t')
     next(tsvreader)
     for row in tsvreader:
         sampleList.append(row[0])
-        templateList.append(row[1])
-        # rotationList.append(row[2])
-
+        templateList.append(row[1:])
+        
 templateList = np.array(templateList, dtype='int')
-# rotationList = np.array(rotationList, dtype='int')
 
-degreesToRotate = 90
-allenSlice = 70
-actSample = "../rawdata/sleepDepBothBatches/sample-07"
 #%% import sample data
+degreesToRotate = 180
+allenSlice = 69
+actSample = "../rawdata/sleepDepBothBatches/sample-09"
 
 sample = importVisiumData(actSample)
-sampleProcessed = processVisiumData(sample)
+template = chooseTemplateSlice(allenSlice)
+sampleProcessed = processVisiumData(sample, template, degreesToRotate)
 sampleMatrix = sample["filteredFeatureMatrix"][2]
 sampleMatrix = sampleMatrix.todense()
-sampleStartingResolution = 0.55 / sample["scaleFactors"]["spot_diameter_fullres"]
-
-template = chooseTemplateSlice(allenSlice)
-# the below is a little superfluous, and the denominator could be changed to .01. keeping it mostly for posterity
-resolutionRatio = sampleStartingResolution / template['startingResolution']
-#%% normalize, resize, and rotate image, think about adding to process function
-sampleNorm = cv2.normalize(sampleProcessed['tissue'], None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-sampleResize = rescale(sampleNorm,resolutionRatio)
-# if resize=True not set, image will be slightly misaligned with spots
-sampleRotate = rotate(sampleResize, degreesToRotate, resize=True)
-sampleHistMatch = match_histograms(sampleRotate, template['leftHem'])
 
 #%% run registration of sample to template
 
 templateAntsImage = ants.from_numpy(template['leftHem'])
-sampleAntsImage = ants.from_numpy(sampleHistMatch)
+sampleAntsImage = ants.from_numpy(sampleProcessed['tissueHistMatched'])
 # templateAntsImage.set_spacing([templateStartingResolution,templateStartingResolution])
 # sampleAntsImage.set_spacing([templateStartingResolution,templateStartingResolution])
 
 synXfm = ants.registration(fixed=templateAntsImage, moving=sampleAntsImage, type_of_transform='SyN', grad_step=0.1, reg_iterations=(60,40,20,0), outprefix=os.path.join(sampleProcessed['derivativesPath'],f"{sample['sampleID']}_xfm"))
 ants.plot(templateAntsImage, overlay=synXfm["warpedmovout"])
 
+# apply syn transform to tissue spot coordinates
+# first line creates a csv file, second line uses that csv as input for antsApplyTransformsToPoints
+np.savetxt(f"{os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_tissuePointsResizeToTemplate.csv",sampleProcessed['tissuePointsForTransform'], delimiter=',', header="x,y,z,t,label,comment")
+os.system(f"antsApplyTransformsToPoints -d 2 -i {os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_tissuePointsResizeToTemplate.csv -o {os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_tissuePointsResizeToTemplateTransformApplied.csv -t [ {os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_xfm0GenericAffine.mat,1] -t {os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_xfm1InverseWarp.nii.gz")
+
 #%%
 # CHECK FOR ACCURACY OF ABOVE REGISTRATION
 #
 
-#%% Tissue point registration
-# update tissue points with pre-registration alignment of sample image
-# -90 = [[0,1],[-1,0]]
-rotMat = [[0,1],[-1,0]]
-if degreesToRotate == 0:
-    rotMat = [[1,0],[0,1]]
-elif degreesToRotate == 90:
-    rotMat = [[0,-1],[1,0]]
-elif degreesToRotate == 270:
-    rotMat = [[0,1],[-1,0]]
-tissuePointsResizeToHighRes = sample["tissuePositionsList"][0:, 3:] * sample["scaleFactors"]["tissue_hires_scalef"]
-# below switches x and y in order to properly rotate, this gets undone in next cell
-tissuePointsResizeToHighRes[:,[0,1]] = tissuePointsResizeToHighRes[:,[1,0]]
-plt.imshow(sampleProcessed["tissue"])
-plt.plot(tissuePointsResizeToHighRes[:,0],tissuePointsResizeToHighRes[:,1],marker='.', c='blue', alpha=0.2)
-plt.show()
 
-tissuePointsResizeRotate = np.matmul(tissuePointsResizeToHighRes, rotMat)
-# below accounts for shift resulting from matrix rotation above, will be different for different angles
-if degreesToRotate == 0:
-    tissuePointsResizeRotate[:,0] = tissuePointsResizeRotate[:,0]
-elif degreesToRotate == 90:
-    tissuePointsResizeRotate[:,1] = tissuePointsResizeRotate[:,1] + sampleProcessed["tissue"].shape[1]
-elif degreesToRotate == 270:
-    tissuePointsResizeRotate[:,0] = tissuePointsResizeRotate[:,0] + sampleProcessed["tissue"].shape[0]
-tissuePointsResizeToTemplate = tissuePointsResizeRotate * resolutionRatio
-plt.imshow(sampleRotate)
-plt.plot(tissuePointsResizeToTemplate[:,0],tissuePointsResizeToTemplate[:,1],marker='.', c='red', alpha=0.2)
-plt.show()
 
-#%% apply syn transform to tissue spot coordinates
-
-# next line reverses x,y switching of above
-tissuePointsResizeToTemplate[:,[0,1]] = tissuePointsResizeToTemplate[:,[1,0]]
-
-csvPad = np.zeros([tissuePointsResizeToTemplate.shape[0],4])
-tissuePointsForTransform = np.append(tissuePointsResizeToTemplate, csvPad, 1)
-np.savetxt(f"{os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_tissuePointsResizeToTemplate.csv",tissuePointsForTransform, delimiter=',', header="x,y,z,t,label,comment")
-os.system(f"antsApplyTransformsToPoints -d 2 -i {os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_tissuePointsResizeToTemplate.csv -o {os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_tissuePointsResizeToTemplateTransformApplied.csv -t [ {os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_xfm0GenericAffine.mat,1] -t {os.path.join(sampleProcessed['derivativesPath'],sample['sampleID'])}_xfm1InverseWarp.nii.gz")
 
 #%% open and check transformed coordinates
 transformedTissuePositionList = []
@@ -253,7 +260,7 @@ with open(os.path.join(f"{os.path.join(sampleProcessed['derivativesPath'],sample
 sampleTransformed = synXfm["warpedmovout"].numpy()
 
 transformedTissuePositionList = np.array(transformedTissuePositionList, dtype=float)
-# again, switching x,y columns back to python compatible
+# switching x,y columns back to python compatible and deleting empty columns
 transformedTissuePositionList[:,[0,1]] = transformedTissuePositionList[:,[1,0]]
 transformedTissuePositionList = np.delete(transformedTissuePositionList, [2,3,4,5],1)
 
@@ -263,7 +270,6 @@ plt.show()
 
 #%% remove any out of bounds points and prepare for comparison to atlas locations
 
-# transformedTissuePositionListMask = transformedTissuePositionList > 0 
 transformedTissuePositionListMask = np.logical_and(transformedTissuePositionList > 0, transformedTissuePositionList < sampleTransformed.shape[0])
 
 transformedTissuePositionListFinal = [];
--- code/spareParts.py
+++ code/spareParts.py
@@ -22,3 +22,47 @@ Created on Tue Mar 15 16:30:34 2022
 
 # hippocampalMask = np.zeros(templateAnnotationLeft.shape)
 # hippocampalMask[templateAnnotationLeft == 1089] = 1
+
+#%% Tissue point registration currently moving into tissue coordinate function
+# update tissue points with pre-registration alignment of sample image
+# -90 = [[0,1],[-1,0]]
+# rotMat = [[0,1],[-1,0]]
+# if degreesToRotate == 0:
+#     rotMat = [[1,0],[0,1]]
+# elif degreesToRotate == 90:
+#     rotMat = [[0,-1],[1,0]]
+# elif degreesToRotate == 180:
+#     rotMat = [[-1,0],[0,-1]]
+# elif degreesToRotate == 270:
+#     rotMat = [[0,1],[-1,0]]
+
+# # scales tissue coordinates down to image resolution
+# tissuePointsResizeToHighRes = sample["tissuePositionsList"][0:, 3:] * sample["scaleFactors"]["tissue_hires_scalef"]
+# # below switches x and y in order to properly rotate, this gets undone in next cell
+# tissuePointsResizeToHighRes[:,[0,1]] = tissuePointsResizeToHighRes[:,[1,0]]
+# plt.imshow(sampleProcessed["tissue"])
+# plt.plot(tissuePointsResizeToHighRes[:,0],tissuePointsResizeToHighRes[:,1],marker='.', c='blue', alpha=0.2)
+# plt.show()
+
+# tissuePointsResizeRotate = np.matmul(tissuePointsResizeToHighRes, rotMat)
+# # below accounts for shift resulting from matrix rotation above, will be different for different angles
+# if degreesToRotate == 0:
+#     tissuePointsResizeRotate[:,0] = tissuePointsResizeRotate[:,0]
+# elif degreesToRotate == 90:
+#     tissuePointsResizeRotate[:,1] = tissuePointsResizeRotate[:,1] + sampleProcessed["tissue"].shape[1]
+# elif degreesToRotate == 180:
+#     tissuePointsResizeRotate[:,0] = tissuePointsResizeRotate[:,0] + sampleProcessed["tissue"].shape[0]
+#     tissuePointsResizeRotate[:,1] = tissuePointsResizeRotate[:,1] + sampleProcessed["tissue"].shape[1]
+# elif degreesToRotate == 270:
+#     tissuePointsResizeRotate[:,0] = tissuePointsResizeRotate[:,0] + sampleProcessed["tissue"].shape[0]
+# tissuePointsResizeToTemplate = tissuePointsResizeRotate * resolutionRatio
+# plt.imshow(sampleRotate)
+# plt.plot(tissuePointsResizeToTemplate[:,0],tissuePointsResizeToTemplate[:,1],marker='.', c='red', alpha=0.2)
+# plt.show()
+
+#%% normalize, resize, and rotate image, think about adding to process function
+# sampleNorm = cv2.normalize(sampleProcessed['tissue'], None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+# sampleResize = rescale(sampleNorm,resolutionRatio)
+# if resize=True not set, image will be slightly misaligned with spots
+# sampleRotate = rotate(sampleResize, degreesToRotate, resize=True)
+# sampleHistMatch = match_histograms(sampleRotate, template['leftHem'])
