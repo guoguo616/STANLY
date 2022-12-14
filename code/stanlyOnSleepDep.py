28fbed8aeb43ef90fce52235e498b2ce4a705b23
zjpeters <zeru-peterson@uiowa.edu>
2022-12-14 11:57:11 -0600

    updates before making changes to sample list import

--- code/stanlyOnSleepDep.py
+++ code/stanlyOnSleepDep.py
@@ -26,8 +26,8 @@ import tables
 import time
 # from scipy.spatial.distance import pdist, squareform, cosine, cdist
 # setting up paths
-derivatives = "/home/zjpeters/rdss_tnj/visiumalignment/derivatives"
-rawdata = "/home/zjpeters/rdss_tnj/visiumalignment/rawdata"
+derivatives = "/home/zjpeters/Documents/visiumalignment/derivatives"
+rawdata = "/home/zjpeters/Documents/visiumalignment/rawdata"
 # next few lines first grabs location of main script and uses that to get the location of the reference data, i.e. one back from teh code folder
 codePath = os.path.realpath(os.path.dirname(__file__))
 refDataPath = codePath.split('/')
@@ -235,9 +235,9 @@ def processVisiumData(visiumData, templateData, rotation):
     denseMatrix = denseMatrix.todense()
     orderedDenseMatrix = denseMatrix[:,tissueSpotBarcodeListSorted]
     countsPerSpot = np.sum(orderedDenseMatrix,axis=0)
-    spotCountMean = np.mean(countsPerSpot)
-    spotCountStD = np.std(countsPerSpot)
-    spotCountZscore = (countsPerSpot - spotCountMean) / spotCountStD
+    countsPerSpotMean = np.mean(countsPerSpot)
+    countsPerSpotStD = np.std(countsPerSpot)
+    spotCountZscore = (countsPerSpot - countsPerSpotMean) / countsPerSpotStD
     spotMask = countsPerSpot > 5000
     spotMask = np.squeeze(np.array(spotMask))
     countsPerGene = np.count_nonzero(np.array(orderedDenseMatrix),axis=1, keepdims=True)
@@ -247,6 +247,8 @@ def processVisiumData(visiumData, templateData, rotation):
     processedVisium['geneListMasked'] = wholeGeneList[geneMask].tolist()
     orderedDenseMatrixSpotMasked = orderedDenseMatrix[:,spotMask]
     orderedDenseMatrixSpotMasked = orderedDenseMatrix[geneMask,:]
+    processedVisium['spotCount'] = orderedDenseMatrixSpotMasked.shape[1]
+    print(f"{processedVisium['sampleID']} has {processedVisium['spotCount']} spots")
     processedVisium['countMaskedTissuePositionList'] = processedVisium['tissuePointsResized'][spotMask,:]
     processedVisium['filteredFeatureMatrixOrdered'] = sp_sparse.csc_matrix(orderedDenseMatrixSpotMasked)
     processedVisium['filteredFeatureMatrixLog2'] = sp_sparse.csc_matrix(np.log2((orderedDenseMatrixSpotMasked + 1)))
@@ -588,12 +590,15 @@ experiment = {'sample-id': np.asarray(sampleList)[imageList],
 #%% import sample data
 
 processedSamples = {}
-
+totalSpotCount = 0
 for actSample in range(len(experiment['sample-id'])):
     sample = importVisiumData(os.path.join(rawdata, experiment['sample-id'][actSample]))
     sampleProcessed = processVisiumData(sample, template, experiment['rotation'][actSample])
     processedSamples[actSample] = sampleProcessed
-
+    totalSpotCount += sampleProcessed['spotCount']
+nTotalSamples = len(processedSamples)
+spotCountMean = totalSpotCount / nTotalSamples
+print(f"Average spotcount for {nTotalSamples} is {spotCountMean}")
 #%% register to "best" sample
 # in this case just the best looking slice
 bestSample = processedSamples[4]
@@ -618,13 +623,13 @@ for actSample in range(len(experimentalResults)):
     
 #%% digital spot creation
 # so far testing has been done at a spot diameter of 18 pixels
-spotDiameter = 18
+spotDiameter = 15
 
 templateDigitalSpots = createDigitalSpots(template, spotDiameter)
 
 kSpots = 7
 nDigitalSpots = len(templateDigitalSpots)
-nTotalSamples = len(allSamplesToAllen)
+# nTotalSamples = len(allSamplesToAllen)
 
 allSampleGeneList = allSamplesToAllen[0]['geneListMasked']
 for i, regSample in enumerate(allSamplesToAllen):        
@@ -754,21 +759,21 @@ for nOfGenesChecked,actGene in enumerate(geneList):
             maxGeneCount = np.nanmax([medianDigitalControl,medianDigitalExperimental])
             # display mean gene count for control group            
             plt.imshow(bestSampleToTemplate['visiumTransformed'],cmap='gray')
-            plt.scatter(templateDigitalSpots[:,0],templateDigitalSpots[:,1], c=np.array(meanDigitalControl[0]), alpha=0.8, vmin=0,vmax=maxGeneCount,plotnonfinite=False,cmap='Reds')
+            plt.scatter(templateDigitalSpots[:,0],templateDigitalSpots[:,1], c=np.array(meanDigitalControl[0]), alpha=0.8, vmin=0,vmax=maxGeneCount,plotnonfinite=False,cmap='Reds',marker='.')
             plt.title(f'Mean gene count for {actGene}, non sleep deprived')
             plt.colorbar()
             plt.savefig(os.path.join(derivatives,f'meanGeneCount{actGene}Control.png'), bbox_inches='tight', dpi=300)
             plt.show()
             # display mean gene count for experimental group
             plt.imshow(bestSampleToTemplate['visiumTransformed'],cmap='gray')
-            plt.scatter(templateDigitalSpots[:,0],templateDigitalSpots[:,1], c=np.array(meanDigitalExperimental[0]), alpha=0.8, vmin=0,vmax=maxGeneCount,plotnonfinite=False,cmap='Reds')
+            plt.scatter(templateDigitalSpots[:,0],templateDigitalSpots[:,1], c=np.array(meanDigitalExperimental[0]), alpha=0.8, vmin=0,vmax=maxGeneCount,plotnonfinite=False,cmap='Reds',marker='.')
             plt.title(f'Mean gene count for {actGene}, sleep deprived')
             plt.colorbar()
             plt.savefig(os.path.join(derivatives,f'meanGeneCount{actGene}SleepDep.png'), bbox_inches='tight', dpi=300)
             plt.show()
             # display t statistics
             plt.imshow(bestSampleToTemplate['visiumTransformed'],cmap='gray')
-            plt.scatter(templateDigitalSpots[:,0],templateDigitalSpots[:,1], c=np.array(actTtest[0]), cmap='seismic',alpha=0.8,norm=zeroCenteredCmap,plotnonfinite=False)
+            plt.scatter(templateDigitalSpots[:,0],templateDigitalSpots[:,1], c=np.array(actTtest[0]), cmap='seismic',alpha=0.8,norm=zeroCenteredCmap,plotnonfinite=False,marker='.')
             plt.title(f't-statistic for {actGene}.')
             plt.colorbar()
             plt.savefig(os.path.join(derivatives,f'tStatGeneCount{actGene}SleepDep.png'), bbox_inches='tight', dpi=300)
@@ -782,133 +787,3 @@ print("--- %s seconds ---" % (time.time() - start_time))
 
 
 
-# #%% extract atlas information
-# from allensdk.core.reference_space_cache import ReferenceSpaceCache
-# reference_space_key = 'annotation/ccf_2017'
-# resolution = 10
-# rspc = ReferenceSpaceCache(resolution, reference_space_key, manifest='manifest.json')
-# rsp = rspc.get_reference_space()
-# #%%
-# # ID 1 is the adult mouse structure graph
-# tree = rspc.get_structure_tree(structure_graph_id=1) 
-# regionList = tree.get_name_map()
-# hippocampus = tree.get_structures_by_name(['Hippocampal formation'])
-
-# hippocampal3dMask = rsp.make_structure_mask([hippocampus[0]['id']])
-# hippocampalMask = hippocampal3dMask[700,:,570:]
-
-# rhinalFissure = tree.get_structures_by_name(['rhinal fissure'])
-# rhinalFissure3dMask = rsp.make_structure_mask([rhinalFissure[0]['id']])
-# rhinalFissureMask = rhinalFissure3dMask[700,:,570:]
-
-# plt.imshow(hippocampalMask)
-# plt.show()
-# bestTemplateSlice10 = bestTemplateSlice * 10
-# plt.imshow(bestSampleToTemplate['visiumTransformed'])
-# plt.imshow(hippocampalMask, alpha=0.3)
-# plt.show()
-
-# plt.imshow(rhinalFissureMask)
-# plt.show()
-# bestTemplateSlice10 = bestTemplateSlice * 10
-# plt.imshow(bestSampleToTemplate['visiumTransformed'])
-# plt.imshow(hippocampalMask, alpha=0.3)
-# plt.show()
-# #%% create digital hippocampal spots
-
-# hippocampalDigitalTemplateSpots = []
-# # spotIdx gives a list of spots within the hippocampal formation
-# spotIdx = []
-# for row in range(len(roundedTemplateSpots)):
-#     # 15 in the following is just to erode around the edge of the brain
-#     if hippocampalMask[roundedTemplateSpots[row,1],roundedTemplateSpots[row,0]] == 1:
-#         hippocampalDigitalTemplateSpots.append(templateSpots[row])
-#         spotIdx.append(row)
-        
-# hippocampalDigitalTemplateSpots = np.array(hippocampalDigitalTemplateSpots)
-# plt.imshow(template['leftHem'])
-# plt.scatter(hippocampalDigitalTemplateSpots[:,0],hippocampalDigitalTemplateSpots[:,1], alpha=0.3)
-# plt.show()
-
-# #%% check for significant genes within a region/list of regions
-# # needs to take an input of regions defined in allen ccf
-# # should be able to use original spots when searching roi
-# # run through entire gene list looking for a change in expression between conditions
-# nSigGenes = 0
-# partialGeneList = list(allSampleGeneList)[0:500]
-# for nOfGenesChecked,actGene in enumerate(partialGeneList):
-#     digitalSamplesControl = []
-#     digitalSamplesExperimental = []
-#     nTestedSamples = 0
-#     nControls = 0
-#     nExperimentals = 0
-#     for actSample in range(nTotalSamples):
-#         try:
-#             geneIndex = allSamplesToAllen[actSample]['geneListMasked'].index(actGene)
-#             spotCheck = np.count_nonzero(allSamplesToAllen[actSample]['filteredFeatureMatrixMasked'][spotIdx,:])
-#             # need to do better than "no expression" because that eliminates the fact that the amount is actually 0, not untested
-            
-#             # use binary mask to remove any tissue spots with no expression
-#             if spotCheck < 15:
-#                 continue
-#             actNN, actCDist = findDigitalNearestNeighbors(hippocampalDigitalTemplateSpots, allSamplesToAllen[actSample]['maskedTissuePositionList'], kSpots)
-#             geneCount = np.zeros([hippocampalDigitalTemplateSpots.shape[0],kSpots])
-#             for spots in enumerate(actNN):
-#                 if ~np.all(spots[1]):
-#                     geneCount[spots[0]] = 0
-#                 else:
-#                     geneCount[spots[0]] = allSamplesToAllen[actSample]['filteredFeatureMatrixMasked'][geneIndex,actNN[spots[0]]]
-                    
-#             geneCount = geneCount.reshape([-1])
-#             nTestedSamples += 1
-#             if truncExperiment['experimental-group'][actSample] == 0:
-#                 digitalSamplesControl.append(geneCount)
-#                 nControls += 1
-#             elif truncExperiment['experimental-group'][actSample] == 1:
-#                 digitalSamplesExperimental.append(geneCount)
-#                 nExperimentals += 1
-                
-#             else:
-#                 continue
-            
-#         except:
-#             continue
-        
-#     if spotCheck < 15:
-#         continue    
-#     digitalSamplesControl = np.array(digitalSamplesControl)
-#     digitalSamplesExperimental = np.array(digitalSamplesExperimental)
-#     maskedTtests = []
-#     allTstats = []
-#     allPvals = []
-#     if ~digitalSamplesControl.any() or ~digitalSamplesExperimental.any():
-#         continue
-#     else:    
-#         actTtest = scipy.stats.ttest_ind(digitalSamplesExperimental,digitalSamplesControl, nan_policy='omit', axis=None)
-#         maskedDigitalCoordinates = []
-#         maskedMeanDigitalControls = []
-#         maskedMeanDigitalExperimentals = []
-#         meanDigitalControlHippocampalFormation = np.mean(digitalSamplesControl)
-#         meanDigitalExperimentalHippocampalFormation = np.mean(digitalSamplesExperimental)
-#         if actTtest[1] <= 0.05:
-#             nSigGenes += 1
-#             maxGeneCount = np.max([meanDigitalControls,meanDigitalExperimentals])
-#             tStatColor = np.full(hippocampalDigitalTemplateSpots.shape[0],actTtest[0])
-#             plt.imshow(bestSampleToTemplate['visiumTransformed'])
-#             if actTtest[0] < 0:
-#                 plt.scatter(hippocampalDigitalTemplateSpots[:,0],hippocampalDigitalTemplateSpots[:,1], c='blue', alpha=0.8,plotnonfinite=False)
-#             else:
-#                 plt.scatter(hippocampalDigitalTemplateSpots[:,0],hippocampalDigitalTemplateSpots[:,1], c='red', alpha=0.8,plotnonfinite=False)
-
-#             # plt.scatter(hippocampalDigitalTemplateSpots[:,0],hippocampalDigitalTemplateSpots[:,1], c=tStatColor, alpha=0.8, vmin=0,vmax=maxGeneCount,plotnonfinite=False)
-#             # plt.title(f'Mean gene count for {actGene}, control')
-#             plt.title(f'{actGene}, non sleep deprived, hippocampal expression p <= 0.05')
-#             plt.colorbar()
-#             plt.show()
-            
-#         else:
-#             continue
-        
-        
-        
-# print("--- %s seconds ---" % (time.time() - start_time))
