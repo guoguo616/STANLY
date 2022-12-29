3714b47d85c6a28cc812f650e1c01ae670312f57
zjpeters <zeru-peterson@uiowa.edu>
2022-12-29 10:48:03 -0600

    cleaned up code folder to remove in development functions and scripts

--- code/runGeneListThroughToppGene.sh
+++ code/runGeneListThroughToppGene.sh
@@ -6,28 +6,31 @@ if [ ! -d $resultsDir/toppGene ]; then
   mkdir $resultsDir/toppGene
 fi
 csvGeneList=$resultsDir/listOfSigSleepDepGenes20221224-162709.csv
-touch $resultsDir/toppGene/geneListForToppGene.txt
-while IFS=, read geneList nSigGenes; do
-  # echo $geneList
-  echo -n "${geneList}," >> $resultsDir/toppGene/geneListForToppGene.txt
-done < $csvGeneList
-for txtFile in $resultsDir/toppGene/geneListForToppGene.txt; do
-  listName=$(echo $txtFile | cut -d / -f 4 | cut -d . -f 1)
-  actList=$(cat $txtFile)
-  callCurl=$(echo "curl -H 'Content-Type: text/json' -d '{\"Symbols\":[\"")
-  for updatedList in ${actList//,/\",\"}; do
-    callCurl="$callCurl$updatedList"
-  done
-  callCurl=$(echo "$callCurl\"]}' https://toppgene.cchmc.org/API/lookup")
-  eval $callCurl > ${resultsDir}/${listName}_entrez_ouput.json
-
-  entrezList=`jq '.Genes[].Entrez' ${resultsDir}/${listName}_entrez_ouput.json`
-  echo $entrezList
-  callApi=$(echo "curl -H 'Content-Type: text/json' -d '{\"Genes\":[")
-  for geneNumber in $entrezList; do
-    callApi="${callApi}${geneNumber},"
-  done
-  callApi=${callApi%,*}
-  callApi=$(echo "${callApi}]}' https://toppgene.cchmc.org/API/enrich?pretty=true")
-  eval $callApi > ${resultsDir}/toppGene/${listName}_func_enrich.json
+txtGeneList=$resultsDir/toppGene/geneListForToppGene.txt
+if [ ! -f $txtGeneList ]; then 
+  touch $txtGeneList
+  while IFS=, read geneList nSigGenes; do
+    # echo $geneList
+    echo -n "${geneList}," >> $txtGeneList
+  done < $csvGeneList
+fi
+# for txtFile in $resultsDir/toppGene/geneListForToppGene.txt; do
+listName=$(echo $txtGeneList | cut -d / -f 4 | cut -d . -f 1)
+actList=$(cat $txtGeneList)
+callCurl=$(echo "curl -H 'Content-Type: text/json' -d '{\"Symbols\":[\"")
+for updatedList in ${actList//,/\",\"}; do
+  callCurl="$callCurl$updatedList"
 done
+callCurl=$(echo "$callCurl\"]}' https://toppgene.cchmc.org/API/lookup")
+eval $callCurl > ${resultsDir}/${listName}_entrez_ouput.json
+
+entrezList=`jq '.Genes[].Entrez' ${resultsDir}/${listName}_entrez_ouput.json`
+echo $entrezList
+callApi=$(echo "curl -H 'Content-Type: text/json' -d '{\"Genes\":[")
+for geneNumber in $entrezList; do
+  callApi="${callApi}${geneNumber},"
+done
+callApi=${callApi%,*}
+callApi=$(echo "${callApi}]}' https://toppgene.cchmc.org/API/enrich?pretty=true")
+eval $callApi > ${resultsDir}/toppGene/${listName}_func_enrich.json
+# done
