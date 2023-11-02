afff9319b2f7ddad490bcac395f2e6dd55af0aa6
Zeru Peterson <zeru-peterson@uiowa.edu>
2023-11-02 10:32:13 -0500

    updated runGeneListThroughToppGene.sh to be a function instead of a script

--- code/runGeneListThroughToppGene.sh
+++ code/runGeneListThroughToppGene.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 # run this after gene lists have been created using the
-resultsDir=../derivatives
-
-if [ ! -d $resultsDir/toppGene ]; then
-  mkdir $resultsDir/toppGene
+if [ $# != 2 ] ; then
+  echo "Usage: `basename $0` {geneList} {outputDirectory}"
+  echo "Searches ToppGene using a .csv file created by STANLY"
+  exit 0;
 fi
-csvGeneList=$resultsDir/listOfSigSleepDepGenesPvalues20230104-161335.csv
-txtGeneList=$resultsDir/toppGene/geneListForToppGene.txt
+csvGeneList=$1
+resultsDir=$2
+# prepare output directory
+if [ ! -d $resultsDir/toppGene ]; then
+  mkdir -p $resultsDir/toppGene
+fi
+# get filename for naming outputs
+filename=$(basename ${csvGeneList%%.csv})
+txtGeneList=$resultsDir/toppGene/${filename}.txt
 if [ ! -f $txtGeneList ]; then
   touch $txtGeneList
-  while IFS=, read geneList nSigGenes; do
-    # echo $geneList
-    echo -n "${geneList}," >> $txtGeneList
-  done < $csvGeneList
+else
+  rm $txtGeneList
+  touch $txtGeneList
 fi
-# for txtFile in $resultsDir/toppGene/geneListForToppGene.txt; do
-listName=$(echo $txtGeneList | cut -d / -f 4 | cut -d . -f 1)
+while IFS=, read geneList nSigGenes; do
+  echo -n "${geneList}," >> $txtGeneList
+done < $csvGeneList
+
 actList=$(cat $txtGeneList)
 callCurl=$(echo "curl -H 'Content-Type: text/json' -d '{\"Symbols\":[\"")
 for updatedList in ${actList//,/\",\"}; do
   callCurl="$callCurl$updatedList"
 done
 callCurl=$(echo "$callCurl\"]}' https://toppgene.cchmc.org/API/lookup")
-eval $callCurl > ${resultsDir}/${listName}_entrez_ouput.json
+eval $callCurl > ${resultsDir}/toppGene/${filename}_entrez_ouput.json
 
-entrezList=`jq '.Genes[].Entrez' ${resultsDir}/${listName}_entrez_ouput.json`
-echo $entrezList
+entrezList=`jq '.Genes[].Entrez' ${resultsDir}/toppGene/${filename}_entrez_ouput.json`
 callApi=$(echo "curl -H 'Content-Type: text/json' -d '{\"Genes\":[")
 for geneNumber in $entrezList; do
   callApi="${callApi}${geneNumber},"
 done
 callApi=${callApi%,*}
 callApi=$(echo "${callApi}]}' https://toppgene.cchmc.org/API/enrich?pretty=true")
-eval $callApi > ${resultsDir}/toppGene/sleep_dep_DEGs_func_enrich.json
-# done
+echo $callApi
+eval $callApi > ${resultsDir}/toppGene/${filename}_func_enrich.json
